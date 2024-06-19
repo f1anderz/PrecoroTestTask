5baf7d35b63bbee533e51b1cdc2de7523b4fe74b
f1anderz <stasstepaniuk12@gmail.com>
2024-06-19 19:24:03 +0300

    init with scss vars

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/style';
+</style>
